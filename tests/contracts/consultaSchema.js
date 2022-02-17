cea97b9c36fc3470ded0e9d58d6050d52e81a457
luiszeh <luisffg91@gmail.com>
2022-02-17 00:18:56 -0300

    identando

--- tests/contracts/consultaSchema.js
+++ tests/contracts/consultaSchema.js
@@ -1,35 +1,16 @@
-const Joi = require('joi');
-
+const Joi = require("joi");
 
 const schema = Joi.object().keys({
-    cep: Joi.string().required(),
-
-    logradouro: Joi.string().allow(''),
-
-    complemento: Joi.string().allow(''),
-
-    bairro: Joi.string().allow(''),
-
-    localidade: Joi.string(),
-
-    uf: Joi.string(),
-
-    ibge: Joi.string(),
-
-    gia: Joi.string().allow(''),
-
-    ddd: Joi.string(),
-
-    siafi: Joi.string()
-})
+  cep: Joi.string().required(),
+  logradouro: Joi.string().allow(""),
+  complemento: Joi.string().allow(""),
+  bairro: Joi.string().allow(""),
+  localidade: Joi.string(),
+  uf: Joi.string(),
+  ibge: Joi.string(),
+  gia: Joi.string().allow(""),
+  ddd: Joi.string(),
+  siafi: Joi.string(),
+});
 
 module.exports = schema;
-
-/* Dúvidas e comentários:
-
-    1 - o cep é possivel realizar a pesquisa com hífen tbm, como testar isso?
-    2 - como utilizar esse schema no assert la no arquivo de teste?
-    3 - o precision é utilizado quando quero um valor que tenha exatamente um numero X de caracteres? ex: igbe que tem que ter 7 digitos
-    4 - Não peguei como faço o assert do joi no arquivo de testes para "linkar" com meu schema e verificar os "matchs"
-
-*/
--- tests/contracts/contract.test.js
+++ tests/contracts/contract.test.js
@@ -1,16 +1,12 @@
-const Joi = require('joi');
-const request = require('supertest');
+const Joi = require("joi");
+const request = require("supertest");
 const API_URL = "https://viacep.com.br/";
 const schema = require("./consultaSchema");
 
-describe('Teste contrato API ViaCep', () => {
+describe("Teste contrato API ViaCep", () => {
+  it("Valida se o status é 200 e se retorna o schema correto", async () => {
+    const response = await request(API_URL).get("ws/25035030/json").expect(200);
 
-    it('Valida se o status é 200 e se retorna o schema correto', async () => {
-
-        const response = await request(API_URL)
-            .get('ws/25035030/json')
-            .expect(200)
-            // const resultado
-            Joi.assert(response.body, schema)
-            })    
-    })
+    Joi.assert(response.body, schema);
+  });
+});
